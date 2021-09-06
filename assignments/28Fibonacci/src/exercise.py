7f34a96e4d3671d6366baa83aa0dbf0b124ca8ed
diegoOlivaH <88850984+diegoOlivaH@users.noreply.github.com>
2021-09-06 14:57:26 -0500

    Update exercise.py

--- assignments/28Fibonacci/src/exercise.py
+++ assignments/28Fibonacci/src/exercise.py
@@ -1,6 +1,20 @@
 
 def main():
     #escribe tu código abajo de esta línea
+    n = int(input("Enter a number: "))
+    a = 0
+    b = 1
+    c = int()
+    d = 1
+    if n==0:
+        print(a)
+    while d<n:
+        c = b + a
+        a = b
+        b = c
+        d += 1
+        if d>=n:
+            print(c)
     pass
 
 if __name__=='__main__':
