443ff8d29feaf91d65a4e9759f1c1e9a4cf63981
diegoOlivaH <88850984+diegoOlivaH@users.noreply.github.com>
2021-09-06 13:33:09 -0500

    Update exercise.py

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -2,6 +2,15 @@
 
 def main():
     #Escribe tu código debajo de esta línea
+    n = int(input("Number: "))
+    v = 1
+    s = int()
+    while s<=n:
+     v = v + 1
+     s = v**2
+     if s>n:
+        print(v)
+
     pass
 
 if __name__=='__main__':
